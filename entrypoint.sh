7fefa71c4d4c1600191479cabcb809215b3ed801
Lukas1818 <git@lukas1818.de>
2020-12-14 18:22:06 +0100

    uncomment build

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,7 +63,7 @@ do_update()
 	date > "$logfile"
 	echo "update package $package_name to $verison" >> "$logfile"
 	set +e
-	#pkgbuild-update --noconfirm --nodeps --pkgname "$package_name" | tee -a "$logfile"
+	pkgbuild-update --noconfirm --nodeps --pkgname "$package_name" | tee -a "$logfile"
 	success=${PIPESTATUS[0]}
 	set -e
 	already_updated="false"
